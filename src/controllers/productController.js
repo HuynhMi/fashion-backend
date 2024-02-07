73c2748beb9670c32bdcb3228f4285db12851354
MiPham <mipham@Mi-Phams-MacBook-Pro.local>
2024-02-07 23:37:46 +0700

    add CRUD

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -2,27 +2,61 @@ const express = require('express');
 const router = express.Router();
 const Product = require('../models/Products');
 
-// Get all products
-router.get('/products', async (req, res) => {
-  try {
-    const products = await Product.find();
-    res.json(products);
-  } catch (error) {
-    res.status(500).json({ error: 'Internal server error' });
-  }
+// Post a single product
+router.post('/product', (req, res) => {
+  console.log(req.body);
+  const products = new Product(req.body);
+
+  products.save()
+    .then (db => res.status(200).send(db))
+    .catch(err => res.status(500).send(err))
 });
 
-// Get a single product by ID
-router.get('/products/:id', async (req, res) => {
-  try {
-    const product = await Product.findById(req.params.id);
-    if (!product) {
-      return res.status(404).json({ error: 'Product not found' });
-    }
-    res.json(product);
-  } catch (error) {
-    res.status(500).json({ error: 'Internal server error' });
-  }
+//read multiple products
+router.get('/products', async (req, res) => {
+  Product.find({}).then(db => {
+    res.send(db);
+  }).catch(err => {
+    res.status(500).send(err)
+  })
 });
+
+//get a single product
+router.get('/product/:id', (req, res) => {
+  const _id = req.params.id;
+  Product.findById(_id)
+    .then(product => {
+      if (!product) return res.status(404).send({ error: 'Product not found' });
+      res.status(200).send(product)
+    })
+    .catch(err => res.status(500).send(err))
+});
+
+//update a single product
+router.patch('/product/:id', (req, res) => {
+  const _id = req.params.id;
+  const _db = req.body;
+  Product.findByIdAndUpdate(_id, _db, {
+    new: true,
+    runValidators: true
+  })
+    .then(product => {
+      if (!product) return res.status(404).send({ error: 'Product not found' });
+      res.status(200).send(product)
+    })
+    .catch(err => res.status(500).send(err))
+});
+
+//delete a single product
+router.delete('/product/:id', (req, res) => {
+  const _id = req.params.id;
+  Product.findByIdAndDelete(_id)
+    .then(product => {
+      if (!product) return res.status(404).send({ error: 'Product not found' });
+      res.status(200).send("Delete successfully")
+    })
+    .catch(err => res.status(500).send(err))
+});
+
 
 module.exports = router;
